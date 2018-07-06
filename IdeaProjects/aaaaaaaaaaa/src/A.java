b0b78fdb2f2237908c9e6f226148e0d7ff25aebb
wyw123qwe <2631285914@qq.com>
2018-07-06 07:35:33 -0400

    q

---
+++ IdeaProjects/aaaaaaaaaaa/src/A.java
@@ -0,0 +1,15 @@
+public class A {
+    private int a;
+
+    public A() {
+
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
